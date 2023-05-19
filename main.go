aaf8a5bda1ad70f387393dc893393ea002fbc1d5
isksss <104404522+isksss@users.noreply.github.com>
2023-05-19 16:53:33 +0900

    add help

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-func findCommandPath(cmd string) (string, error) {
-	return exec.LookPath(cmd)
-}
+func findCommandPath(cmd string) (string, error) {  
+  return exec.LookPath(cmd)  
+}  
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: which <command>")
-		os.Exit(1)
-	}
-	
-	cmd := os.Args[1]
+func main() {  
+  help := flag.Bool("help", false, "display help message")
+  flag.Parse()
+  
+  if *help || len(flag.Args()) < 1 {  
+    fmt.Fprintln(os.Stderr, "Usage: which <command>")  
+    fmt.Fprintln(os.Stderr, "Flags:")
+    fmt.Fprintln(os.Stderr, "  --help: display this help message")
+    os.Exit(1)
+  }  
 
-	path, err := findCommandPath(cmd)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
+  cmd := flag.Arg(0)
 
-	fmt.Printf("%s\n", path)
+  path, err := findCommandPath(cmd)  
+  if err != nil {  
+    fmt.Fprintf(os.Stderr, "Error: %v\n", err)  
+    os.Exit(1)  
+  }  
+
+  fmt.Printf("%s\n", path)  
 }
